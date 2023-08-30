b422b6c9719626e3c8b301ab0e7dbc578140d96d
Usman Habib <usmanhabib802@gmail.com>
2023-08-30 13:55:28 +0500

    Initial Commit

---
+++ chapter_1/sandbox.js
@@ -0,0 +1,13 @@
+// object literals
+
+let user = {
+    name : "Cris",
+    age : 20,
+    email : "Cris@111gmail.com",
+    address : "USA",
+    blogs : ["Why u follow rules" , "Just do whatever u feel right"]
+};
+
+console.log(user);
+console.log(user.name);
+
