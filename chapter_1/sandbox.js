7c041655d88d3fd7610aac6888906cf75d84cc6d
Usman Habib <usmanhabib802@gmail.com>
2023-09-01 18:13:21 +0500

    final commit

--- chapter_1/sandbox.js
+++ chapter_1/sandbox.js
@@ -1,15 +1,10 @@
-const para = document.querySelector('p');
-console.log(para.innerText);
-para.innerText = "Ninja are Awosome!";
-console.log(para.innerText);
 const paras = document.querySelectorAll('p');
 
-paras.forEach(para => {
-    console.log(para.innerText);
-    para.innerText += " what you want";
-});
-
-const content = document.querySelector('.content');
-console.log(content.innerHTML);
-console.log(content.innerText);
-content.innerHTML += '<h2>THIS IS A NEW TAG</h2>';
+paras.forEach(p => {
+    if(p.textContent.includes("error")){
+        p.classList.add('error');
+    }
+    if(p.textContent.includes("error")){
+        p.classList.add('success');
+    }
+})
